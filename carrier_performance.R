43f4bdcf64cb20b7c1af47e6964ea5f6c6917bd1
Ethan Casto <castoe24@mail.wlu.edu>
2023-11-01 16:17:49 -0400

    added multiselect but it has some bugs to be worked out. the 3rd plot now either shows a graph  or says "data unavailable" depending on what input you select

--- carrier_performance.R
+++ carrier_performance.R
@@ -14,55 +14,66 @@ delay_types <- readRDS('data/delay_types.rds')
 carrier_carriers <- readRDS("data/carriers.rds")
 
 carrier_performance <- tabPanel("Carrier Performance", 
-                        selectInput('selectCarrier', 'Aircraft Carrier',
-                            choices = carrier_carriers),
+                        selectInput('selectCarrier', 
+                                    'Aircraft Carrier',
+                                    multiple = TRUE,
+                                    choices = carrier_carriers),
                         plotOutput('arr_delayPlot'),
                         plotOutput('delay_typesPlot'),
-                        plotOutput('reviewsPlot'))
+                        uiOutput('reviewsPlot'))
 
 carrier_performance_arr_delay <- function(input) { 
-  renderPlot({
-    gc()
-    lapply(input$selectCarrier, function(x) { 
-      readRDS(paste0("data/", x, ".rds")) 
-    }) %>%
-      do.call(rbind, .) %>%
-      ggplot(aes(ARR_DELAY)) + 
-      geom_density() + 
-      ggtitle('DENSITY OF ARRIVAL DELAY') + 
-              xlab('ARRIVAL DELAY IN MINUTES') +
-              ylab('DENSITY')
-  })
+    renderPlot({
+        gc()
+        lapply(input$selectCarrier, function(x) { 
+            readRDS(paste0("data/", x, ".rds")) 
+        }) %>%
+            do.call(rbind, .) %>%
+            ggplot(aes(ARR_DELAY),
+                   color = OP_CARRIER) + 
+                geom_density() + 
+                ggtitle('DENSITY OF ARRIVAL DELAY') + 
+                xlab('ARRIVAL DELAY IN MINUTES') +
+                ylab('DENSITY')
+    })
 }
 
 carrier_performance_delay_types <- function(input) {
-  renderPlot({
-    delay_types %>%
-      filter(OP_CARRIER == input$selectCarrier) %>%
-      gather('DELAY_TYPE', 
-             'COUNTS',
-             'CARRIER_DELAY':'LATE_AIRCRAFT_DELAY') %>%
-      ggplot(aes(DELAY_TYPE, COUNTS)) + 
-      geom_bar(stat = 'identity') + 
-      ggtitle('NUMBER OF DELAY TYPES') +
-              xlab('DELAY TYPES') +
-              ylab('NUMBER')
-  })
+    renderPlot({
+        delay_types %>%
+            filter(OP_CARRIER %in% input$selectCarrier) %>%
+            gather('DELAY_TYPE', 
+                  'COUNTS',
+                  'CARRIER_DELAY':'LATE_AIRCRAFT_DELAY') %>%
+            ggplot(aes(DELAY_TYPE, 
+                       COUNTS,
+                       color = OP_CARRIER)) + 
+                geom_bar(stat = 'identity') + 
+                ggtitle('COUNTS OF DELAY TYPES') +
+                xlab('DELAY TYPES') +
+                ylab('COUNTS')
+    })
 }
 
 carrier_performance_reviews <- function(input) {
-  renderPlot({
-    reviews %>%
-      filter(OP_CARRIERS == input$selectCarrier) %>%
-      ggplot(aes(REVIEWS)) + 
-      geom_density() + 
-      scale_x_continuous(breaks = 1:10, 
-                         labels = 1:10) +
-      ggtitle('DENSITY OF REVIEWS (1-10)') +
-              xlab('REVIEWS (1-10)') +
-              ylab('DENSITY')
-  })
+    renderUI({
+        a <- c('NK', 'WN', 'AA', 'DL', 'UA', 'AS', 'B6', 'F9')
+        if (a %in% input$selectCarrier) {
+            renderPlot({
+                reviews %>%
+                filter(OP_CARRIERS == input$selectCarrier) %>%
+                ggplot(aes(REVIEWS),
+                       color = OP_CARRIERS) +
+                    geom_density() +
+                    scale_x_continuous(breaks = 1:10, 
+                                      labels = 1:10) +
+                    ggtitle('DENSITY OF REVIEWS (1-10)') +
+                    xlab('REVIEWS (1-10)') +
+                    ylab('DENSITY')
+            })
+        } else { 
+          print('DATA UNAVAILABLE')   
+        }
+})
 }
                                 
-                                
-
--- server.R
+++ server.R
@@ -9,17 +9,15 @@ source("delay_predictor.R")
 source("carrier_performance.R")
 
 function(input, output, session) {
-  
-
-
 
 observe({ output$delay_expected_table <- delay_expected_table(input) })
   
 output$arr_delayPlot <- carrier_performance_arr_delay(input)
+
 output$delay_typesPlot <- carrier_performance_delay_types(input)
+
 output$reviewsPlot <- carrier_performance_reviews(input)
 
-  
 output$flight_route_map <- flight_route_map(input)
 
 }
